d8ea3a1d809c2ab0a5cbaf27d41aae19e3ae8693
Pav <pav0107@gmail.com>
2022-06-02 23:02:34 +0100

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   mode: 'jit',
+  purge: ['./pages/**/*.html', './components/**/*.{js,jsx,ts,tsx}'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -7,5 +8,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
---
+++ utils/requests.js
@@ -0,0 +1,48 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+  fetchTrending: {
+    title: 'Trending',
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: 'Top Rated',
+    url: `/moive/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: 'Action',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
+  fetchComedyMovies: {
+    title: 'Comedy',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  fetchHorrorMovies: {
+    title: 'Horror',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchRomanceMovies: {
+    title: 'Romance',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  fetchMystery: {
+    title: 'Mystery',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
+  fetchSciFi: {
+    title: 'Sci-Fi',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=878`,
+  },
+  fetchWestern: {
+    title: 'Western',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=37`,
+  },
+  fetchAnimation: {
+    title: 'Animation',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=16`,
+  },
+  fetchTV: {
+    title: 'TV Movie',
+    url: `/Discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  },
+};
