d4b1475c7182534e2dced6f362606799b299cf84
Pav <pav0107@gmail.com>
2022-06-02 21:29:41 +0100

    set up header container

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
@@ -8,9 +9,9 @@ export default function Home() {
         <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hulu clone</h1>
 
       {/* Header */}
+      <Header />
 
       {/* Nav */}
 
