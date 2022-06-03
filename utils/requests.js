4c5595950d8dcaa19e7737b967c07a096f275e36
Pav <pav0107@gmail.com>
2022-06-03 21:35:38 +0100

    fix bug when loading different genres

--- utils/requests.js
+++ utils/requests.js
@@ -11,38 +11,38 @@ export default {
   },
   fetchActionMovies: {
     title: 'Action',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=28`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
     title: 'Comedy',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=35`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: 'Horror',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=27`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: 'Romance',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   },
   fetchMystery: {
     title: 'Mystery',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
   },
   fetchSciFi: {
     title: 'Sci-Fi',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=878`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
   },
   fetchWestern: {
     title: 'Western',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=37`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
   },
   fetchAnimation: {
     title: 'Animation',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=16`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
   },
   fetchTV: {
     title: 'TV Movie',
-    url: `/Discover/movie?api_key=${API_KEY}&with_genres=10770`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
   },
 };
